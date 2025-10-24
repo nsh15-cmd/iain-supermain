5766125fe60049b555e51531aced9881952b0285
nsh15-cmd <pinlacnishia@gmail.com>
2025-10-25 03:26:09 +0800

    mimit

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,6 +2,10 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
 const Input = React.forwardRef<
   HTMLInputElement,
   React.InputHTMLAttributes<HTMLInputElement>
